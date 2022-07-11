e56de1e31080af793c65dcf89e795cbdfcdcce52
Arina Ryazanova <75028880+ArinaRy@users.noreply.github.com>
2022-07-11 17:42:15 +0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Sidebar from './components/Sidebar/Sidebar'
+import './App.css'
+
+const App = () => {
+  return (
+    <div id='top'>
+      <Header />
+      <Sidebar />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
